2d3a2b20ee6eb1b19a2e021fda70cde904140f13
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-06-03 00:48:49 +0530

    Pulumi app

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+
+import pulumi.automation as auto
 
 
-app = Flask(__name__)
+def ensure_plugins():
+    ws = auto.LocalWorkspace()
+    ws.install_plugin("aws", "v4.0.0")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
 
-if __name__ == "__main__":
-    app.run()
+def create_app():
+    ensure_plugins()
+    app = Flask(__name__, instance_relative_config=True)
+    aws.config.from_mapping(
+        SECRET_KEY="secret",
+        PROJECT_NAME="fictional-goggles",
+        PULUMI_ORG=os.environ.get("PULUMI_ORG"),
+    )
+
+    @app.route("/", methods=["GET"])
+    def index():
+        return render_template("index.html")
+
+    import sites
+
+    app.register_blueprint(sites.bp)
+
+    import virtual_machines
+
+    app.register_blueprint(virtual_machines.bp)
+
+    return app
